921e6075b8c573b23238a203b21d9474774bdb84
rikato <rikato@live.nl>
2021-08-10 02:16:29 +0200

    added custom objs

--- GpFinalAssignment/src/World/CustomDesigned/UniverseBoard.h
+++ GpFinalAssignment/src/World/CustomDesigned/UniverseBoard.h
@@ -5,9 +5,9 @@ class UniverseBoard : public Object
 {
 public:
 	UniverseBoard(glm::vec3 translation = glm::vec3(0, 0, 0), glm::vec3 rotation = glm::vec3(0, 0, 0)) : Object(
-		new Mesh("assets/models/universeBoard/universeBoard.3dobj"),
+		new Mesh("assets/models/universe_board/universeBoard.3dobj"),
 		new Shader(""),
-		new Material("assets/models/universeBoard/universeBoard.bmp"))
+		new Material("assets/models/universe_board/universeBoard.bmp"))
 	{
 		m_Transform = glm::rotate(m_Transform, rotation.x, glm::vec3(1, 0, 0));
 		m_Transform = glm::rotate(m_Transform, rotation.y, glm::vec3(0, 1, 0));
