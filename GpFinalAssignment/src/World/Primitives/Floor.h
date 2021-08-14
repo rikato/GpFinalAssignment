b582e12af68361716a62e2df13f935f92f10ab18
rikato <rikato@live.nl>
2021-08-14 12:59:46 +0200

    fixed floor primitive mesh

--- GpFinalAssignment/src/World/Primitives/Floor.h
+++ GpFinalAssignment/src/World/Primitives/Floor.h
@@ -6,33 +6,35 @@ class Floor : public Object
 public:
 	Floor(glm::vec3 translation = glm::vec3(0, 0, 0)) : Object(
 		new Mesh(
-			// Vertices.
 			{
-				glm::vec3(0, 0, 0),
-				glm::vec3(0, 0, 1),
-				glm::vec3(1, 0, 1),
-				glm::vec3(1, 0, 1),
-				glm::vec3(0, 0, 0),
-				glm::vec3(1, 0, 0)
-			},
-			// Normals.
-			{
-				glm::vec3(0, 1, 0),
-				glm::vec3(0, 1, 0),
-				glm::vec3(0, 1, 0),
-				glm::vec3(0, 1, 0),
-				glm::vec3(0, 1, 0),
-				glm::vec3(0, 1, 0)
-			},
-			// Uvs.
-			// Tile the texture 10 * 10 times over the x and y axis.
-			{ 
-				glm::vec2(10, 10),
-				glm::vec2(10, 0),
-				glm::vec2(0, 0),
-				glm::vec2(0, 0),
-				glm::vec2(10, 10),
-				glm::vec2(0, 10) 
+				// Vertices.
+				{
+					glm::vec3(0, 0, 0),
+					glm::vec3(0, 0, 1),
+					glm::vec3(1, 0, 1),
+					glm::vec3(1, 0, 1),
+					glm::vec3(0, 0, 0),
+					glm::vec3(1, 0, 0)
+				},
+				// Normals.
+				{
+					glm::vec3(0, 1, 0),
+					glm::vec3(0, 1, 0),
+					glm::vec3(0, 1, 0),
+					glm::vec3(0, 1, 0),
+					glm::vec3(0, 1, 0),
+					glm::vec3(0, 1, 0)
+				},
+				// Uvs.
+				// Tile the texture 10 * 10 times over the x and y axis.
+				{
+					glm::vec2(10, 10),
+					glm::vec2(10, 0),
+					glm::vec2(0, 0),
+					glm::vec2(0, 0),
+					glm::vec2(10, 10),
+					glm::vec2(0, 10)
+				} 
 			}
 		),
 		new Shader(""),
