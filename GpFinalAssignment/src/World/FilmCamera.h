7f1ee735fd071383b30841188d0d612bc20fd955
rikato <rikato@live.nl>
2021-08-15 01:29:06 +0200

    added custom objs

---
+++ GpFinalAssignment/src/World/FilmCamera.h
@@ -0,0 +1,21 @@
+#include "Object.h"
+
+class FilmCamera : public Object
+{
+public:
+	FilmCamera(glm::vec3 translation = glm::vec3(0, 0, 0), glm::vec3 rotation = glm::vec3(0, 0, 0)) : Object(
+		new Mesh("assets/models/film_camera/film_camera.3dobj"),
+		new Shader("assets/shaders/vertex.shader", "assets/shaders/fragment.shader"),
+		new Material())
+	{
+		Texture* diffuse = new Texture(DIFFUSE, "assets/models/film_camera/film_camera.bmp", "diffuseMap", *m_Shader);
+
+		m_Material->AddTexture(diffuse);
+
+		m_LocalTransform = glm::rotate(m_LocalTransform, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+		m_LocalTransform = glm::rotate(m_LocalTransform, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+		m_LocalTransform = glm::rotate(m_LocalTransform, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+
+		m_LocalTransform = glm::translate(m_LocalTransform, translation);
+	}
+};
