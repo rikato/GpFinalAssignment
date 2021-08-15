8d6f38ce63806682c2e28c2271b9761c47db96c1
rikato <rikato@live.nl>
2021-08-15 11:43:01 +0200

    lowered poly count moon surface and changed floor sign y

--- GpFinalAssignment/src/Scene/Scene.cpp
+++ GpFinalAssignment/src/Scene/Scene.cpp
@@ -22,23 +22,23 @@ Scene::Scene(GLFWwindow *window)
 	m_Camera = new Camera(glm::vec3(0, 0, 0), window);
 
 	// Scene objects (6 uniques).
-	//MoonSurface* moonSurface = new MoonSurface(glm::vec3(0, 0.28, 0));
+	MoonSurface* moonSurface = new MoonSurface(glm::vec3(0, 0.28, 0));
 	Lander* lander = new Lander(glm::vec3(0, 2.49, 3));
 	PulledCosmonaut* buzz = new PulledCosmonaut(glm::vec3(1.27, 0.07, 0), glm::vec3(0, 45, 0));
 	Cosmonaut* neil = new Cosmonaut(glm::vec3(-1.27, 0.07, 0));
 	Spotlight* spotlightLeft = new Spotlight(glm::vec3(-1.3, 0, -5.7));
 	Spotlight* spotlightRight = new Spotlight(glm::vec3(1.3, 0, -5.7));
-    FilmCamera* filmCamera = new FilmCamera(glm::vec3(0, 1, 0), glm::vec3(0, 180, 0));
+	FilmCamera* filmCamera = new FilmCamera(glm::vec3(0, 1, 0), glm::vec3(0, 180, 0));
 	// DirectorChair* directorChair = new DirectorChair(glm::vec3(0, 0, 0));
 
 	// Custom designed objects (5 uniques).
 	Flag* flag = new Flag(glm::vec3(0, .5, -0.5));
 	UniverseBoard* universeBoard = new UniverseBoard(glm::vec3(0, 3, 0));
-	WetFloorSign* wetFloorSign = new WetFloorSign(glm::vec3(-4, 0.434748, 0), glm::vec3(0, -45, 0));
+	WetFloorSign* wetFloorSign = new WetFloorSign(glm::vec3(-4, 0.5, 0), glm::vec3(0, -45, 0));
 	MovieClapper* movieClapper = new MovieClapper(glm::vec3(1, 0.202, 0));
 	VoiceCone* voiceCone = new VoiceCone(glm::vec3(1.4, 0.25, 0));
 
-	// Custom primitive meshes (4).
+	// Custom primitive meshes (5 uniques).
 	Floor* floor = new Floor();
 	Earth* earth= new Earth(glm::vec3(4, 1.5, -0.5));
 	Rope* rope = new Rope(glm::vec3(0, 2, -0.4), glm::vec3(90, 0, 0));
@@ -48,7 +48,7 @@ Scene::Scene(GLFWwindow *window)
 	NuclearWasteSign* nuclearWasteSign = new NuclearWasteSign(glm::vec3(0, 3, -1.4), glm::vec3(0, 45, 0));
 
 	// The whole set that is staging the moon landing.
-	Model* moonLandingSet = new Model({  lander, buzz, neil, spotlightLeft, spotlightRight, flag }, glm::vec3(0, 0, 11));
+	Model* moonLandingSet = new Model({ moonSurface, lander, buzz, neil, spotlightLeft, spotlightRight, flag }, glm::vec3(0, 0, 11));
 
 	// The fake earth hanging from the roof consisting of 4 primitve meshes.
 	Model* fakeUniverse = new Model({ universeBoard, rope, earth }, glm::vec3(0, 0, 20));
